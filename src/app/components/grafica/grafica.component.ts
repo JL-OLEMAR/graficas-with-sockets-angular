06c0ee7753f3b368b36b682de515119355d68210
JL-OLEMAR <joseluis19963@gmail.com>
2021-11-23 16:55:18 -0500

    Mostrar los nuevos valores de la grafica

--- src/app/components/grafica/grafica.component.ts
+++ src/app/components/grafica/grafica.component.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http'
 import { Component, OnInit } from '@angular/core'
 import { ChartDataSets } from 'chart.js'
 import { Label } from 'ng2-charts'
+import { WebsocketService } from '../../services/websocket.service'
 
 @Component({
   selector: 'app-grafica',
@@ -15,15 +16,25 @@ export class GraficaComponent implements OnInit {
 
   public lineChartLabels: Label[] = ['January', 'February', 'March', 'April']
 
-  constructor (private readonly http: HttpClient) { }
+  constructor (
+    private readonly http: HttpClient,
+    public wsService: WebsocketService
+  ) { }
 
   ngOnInit (): void {
     this.getData()
+    this.escucharSocket()
   }
 
   // Mostrar datos en la gráfica desde el servidor
   getData (): any {
     this.http.get('http://localhost:5000/grafica')
+      .subscribe((data: any) => (this.lineChartData = data))
+  }
+
+  // Escuchar el evento 'cambio-grafica' del socket
+  escucharSocket (): void {
+    this.wsService.listen('cambio-grafica')
       .subscribe((data: any) => (this.lineChartData = data))
   }
 }
