da29df8db1352456637cdf3245ce555f0322da15
JL-OLEMAR <joseluis19963@gmail.com>
2021-11-23 11:12:07 -0500

    change ng2-charts@2.2.3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core'
 import { BrowserModule } from '@angular/platform-browser'
 import { SocketIoModule, SocketIoConfig } from 'ngx-socket-io'
+import { ChartsModule } from 'ng2-charts'
 import { AppComponent } from './app.component'
 
 const config: SocketIoConfig = { url: 'http://localhost:5000', options: {} }
@@ -11,7 +12,8 @@ const config: SocketIoConfig = { url: 'http://localhost:5000', options: {} }
   ],
   imports: [
     BrowserModule,
-    SocketIoModule.forRoot(config)
+    SocketIoModule.forRoot(config),
+    ChartsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
